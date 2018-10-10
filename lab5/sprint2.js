9f80bcde0efa5b6d5837900ac9abf9100028f8fe
ManuellaG <31465996+ManuellaG@users.noreply.github.com>
2018-10-10 18:30:04 +0300

    Add files via upload

--- lab5/sprint2.js
+++ lab5/sprint2.js
@@ -4,9 +4,10 @@ $('img').on({
         if(i>5){
         i=1;
         }
+        if($(this).attr("src")=="6.jpg"){
         var j=i+".jpg";
         $(this).attr('src',j);
         i=i+1;
-    
+    }
     }
 });
